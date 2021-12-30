250a01d080c3a5f67485d6a42d4e8fb37cf6f648
N-Tandiono <n.tandiono@yahoo.com>
2021-12-30 21:11:43 +1100

    added two important tests, 1 required to be fixed

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -43,3 +43,111 @@ def test_cards_players_sample(setup):
     setup._table.add_table_cards(Card("Diamonds", "Ace"))
     
     assert(setup.reveal() == {rank.STRAIGHT: [a], rank.PAIR: [b]})
+
+def test_chips_after_round(setup):
+    '''
+    Players compared chips are game scenario
+    '''
+    a = HumanPlayer("Player 1", 1000)
+    a.give_card(Card("Diamonds", "8"))
+    a.give_card(Card("Spades", "9"))
+    
+    b = HumanPlayer("Player 2", 1000)
+    b.give_card(Card("Diamonds", "5"))
+    b.give_card(Card("Diamonds", "10"))
+
+    a._chips -= 100
+    b._chips -= 100
+
+    a._round_bet += 100
+    b._round_bet += 100
+
+    setup._table.pot = 200
+
+    setup._table.attach(a)
+    setup._table.attach(b)
+    
+    setup._table.add_table_cards(Card("Diamonds", "5"))
+    setup._table.add_table_cards(Card("Spades", "6"))
+    setup._table.add_table_cards(Card("Spades", "7"))
+    setup._table.add_table_cards(Card("Hearts", "King"))
+    setup._table.add_table_cards(Card("Diamonds", "Ace"))
+    
+    setup.reveal()
+
+    assert(setup._table.pot == 0)
+    assert(a._chips == 1100)
+    assert(b._chips == 900)
+    assert(a._chips + b._chips == 2000)
+
+def test_chips_after_round(setup):
+    '''
+    Players compared chips are game scenario
+    '''
+    a = HumanPlayer("Player 1", 1000)
+    a.give_card(Card("Diamonds", "8"))
+    a.give_card(Card("Spades", "9"))
+    
+    b = HumanPlayer("Player 2", 1000)
+    b.give_card(Card("Diamonds", "5"))
+    b.give_card(Card("Diamonds", "10"))
+
+    a._chips -= 100
+    b._chips -= 100
+
+    a._round_bet += 100
+    b._round_bet += 100
+
+    setup._table.pot = 200
+
+    setup._table.attach(a)
+    setup._table.attach(b)
+    
+    setup._table.add_table_cards(Card("Diamonds", "5"))
+    setup._table.add_table_cards(Card("Spades", "6"))
+    setup._table.add_table_cards(Card("Spades", "7"))
+    setup._table.add_table_cards(Card("Hearts", "King"))
+    setup._table.add_table_cards(Card("Diamonds", "Ace"))
+    
+    setup.reveal()
+
+    assert(setup._table.pot == 0)
+    assert(a._chips == 1100)
+    assert(b._chips == 900)
+    assert(a._chips + b._chips == 2000)
+
+def test_chips_after_round_higher_card(setup):
+    '''
+    Players compared chips are game scenario
+    '''
+    a = HumanPlayer("Player 1", 1000)
+    a.give_card(Card("Diamonds", "10"))
+    a.give_card(Card("Spades", "4"))
+    
+    b = HumanPlayer("Player 2", 1000)
+    b.give_card(Card("Diamonds", "4"))
+    b.give_card(Card("Diamonds", "3"))
+
+    a._chips -= 100
+    b._chips -= 100
+
+    a._round_bet += 100
+    b._round_bet += 100
+
+    setup._table.pot = 200
+
+    setup._table.attach(a)
+    setup._table.attach(b)
+    
+    setup._table.add_table_cards(Card("Diamonds", "Ace"))
+    setup._table.add_table_cards(Card("Spades", "King"))
+    setup._table.add_table_cards(Card("Spades", "Queen"))
+    setup._table.add_table_cards(Card("Hearts", "2"))
+    setup._table.add_table_cards(Card("Diamonds", "6"))
+    
+    setup.reveal()
+
+    assert(setup._table.pot == 0)
+    assert(a._chips == 1100)
+    assert(b._chips == 900)
+    assert(a._chips + b._chips == 2000)
